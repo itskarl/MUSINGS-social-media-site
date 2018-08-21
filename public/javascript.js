e6ce0d202d32e290fa8d393990f67d831c96c3dd
Karl Rodulfo <karlrodulfo@gmail.com>
2018-08-21 16:22:25 -0400

    so much progress so much more to do

---
+++ public/javascript.js
@@ -0,0 +1,16 @@
+// CAROUSEL CODE from LIBRARY CREDITS BELOW
+
+
+  // Original JavaScript code by Chirp Internet: www.chirp.com.au
+  // Please acknowledge use of this code by including this header.
+
+  window.addEventListener("DOMContentLoaded", function(e) {
+
+    var stage = document.getElementById("stage");
+    var fadeComplete = function(e) { stage.appendChild(arr[0]); };
+    var arr = stage.getElementsByTagName("a");
+    for(var i=0; i < arr.length; i++) {
+      arr[i].addEventListener("animationend", fadeComplete, false);
+    }
+
+  }, false);
